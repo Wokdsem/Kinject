037e72e7092691c9b32eca59eb264d05ca7a0386
wokdsem <wokdsem@gmail.com>
2016-12-06 14:53:15 +0100

    (In progress) [Kinject 1.2.0] Assert kinject.inject arguments

--- kinject/src/main/java/com/wokdsem/kinject/core/KinjectEngine.java
+++ kinject/src/main/java/com/wokdsem/kinject/core/KinjectEngine.java
@@ -2,7 +2,6 @@ package com.wokdsem.kinject.core;
 
 import com.wokdsem.kinject.core.binder.Binder;
 import com.wokdsem.kinject.support.Locker;
-
 import java.util.HashMap;
 import java.util.Map;
 
@@ -21,6 +20,7 @@ class KinjectEngine implements Injector {
 	@Override
 	@SuppressWarnings({ "unchecked", "ConstantConditions" })
 	public <T> T inject(Class<T> tClass, String named) {
+		assertArguments(tClass, named);
 		String key = getInternalKey(tClass, named);
 		Binder tBinder = binders.get(key);
 		if (tBinder == null) {
@@ -43,11 +43,22 @@ class KinjectEngine implements Injector {
 		return String.format("%s@%s", tClass.getCanonicalName(), named);
 	}
 
+	private static void assertArguments(Class<?> tClass, String named) {
+		if (tClass == null || named == null) {
+			onArgumentError("Trying to inject with a null class or named argument.");
+		}
+	}
+
 	private static void assertBinder(Binder binder, String key) {
 		if (binder == null) {
-			String errMsg = String.format("Kinject [ERROR] - %s is not a valid injectable.", key);
-			throw new IllegalArgumentException(errMsg);
+			String errMsg = String.format("%s is not a valid injectable.", key);
+			onArgumentError(errMsg);
 		}
+	}
+
+	private static void onArgumentError(String errMsg) {
+		String illegalErrorMsg = String.format("Kinject [ERROR] - %s", errMsg);
+		throw new IllegalArgumentException(illegalErrorMsg);
 	}
 
 }
